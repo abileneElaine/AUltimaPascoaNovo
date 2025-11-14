5727c536972bdac348b615dce83cf191e13d1539
abileneElaine <145374967+abileneElaine@users.noreply.github.com>
2025-11-14 11:52:02 -0300

    naofoi

--- pascoazinha/Assets/Scripts/Inimigo.cs
+++ pascoazinha/Assets/Scripts/Inimigo.cs
@@ -4,46 +4,40 @@ public class Inimigo : MonoBehaviour
 {  
     public float speed;
     public bool ground = true;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
+    public Transform groundCheck; 
+    public LayerMask groundLayer; 
     public bool facingRight = true;
 
-    [Header("Componentes")]
-    public Animator animator;  // <<< ADICIONADO
-
-    private void Update()
+    void Start()
     {
-        // --- Movimento ---
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        
+    }
 
-        // --- Checar borda ---
-        ground = Physics2D.Linecast(groundCheck.position, transform.position, groundLayer);
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        ground = Physics2D.Linecast(groundCheck.position, transform.position,groundLayer);
+        Debug.Log(ground);
+
 
         if (ground == false)
         {
             speed *= -1;
         }
 
-        // --- Flip ---
         if (speed > 0 && !facingRight)
         {
             Flip();
         }
+        
         else if (speed < 0 && facingRight)
         {
             Flip();
         }
-
-        // --- Ativar animação walk ---
-        bool andando = Mathf.Abs(speed) > 0.1f;  
-        animator.SetBool("walk", andando);
-    }
-
-    void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 Scale = transform.localScale;
-        Scale.x *= -1;
-        transform.localScale = Scale;
-    }
+    }    
+        void Flip() 
+        { facingRight = !facingRight; Vector3 Scale = transform.localScale; Scale.x *= -1; 
+          transform.localScale = Scale; 
+        }
+    
 }
